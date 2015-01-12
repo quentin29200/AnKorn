ce0b71fbaa6ba94fe6a144b3e2906215faad39a7
quentin29200 <quentin.lgdc@gmail.com>
2015-01-12 22:15:28 +0100

    Recup BDD User

--- src/PA/UserBundle/Controller/DefaultController.php
+++ src/PA/UserBundle/Controller/DefaultController.php
@@ -4,10 +4,30 @@ namespace PA\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use PA\UserBundle\Entity\User;
+use PA\UserBundle\Form\UserType;
+
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('PAUserBundle:layout.html.twig');
+    }
+
+    public function modifierUserAction()
+  	{
+        $em = $this->getDoctrine()->getManager();
+
+        // Récupération de l'ID de l'utilisateur
+        $userid = $this->container->get('security.context')->getToken()->getUser()->getId();
+
+        // Récupération de l'user
+        $user = $em->getRepository('PAUserBundle:User')->find($userid);
+        
+        return $this->render('PAUserBundle:User:monCompte.html.twig', array('user' => $user));
+        	
     }
 }
